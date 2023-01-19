05b49815c485670f993617700570b3505464445e
Mark Page <ping@markpage.net>
2023-01-20 00:19:16 +0100

    Adds ww_status rows to version control

---
+++ rows.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `ww_status` (`id`, `hidden`, `status`, `name`, `notes`) VALUES
+(1,	0,	'R',	'Raised (still in the location of origin)',	''),
+(2,	0,	'T',	'In transit (between locations)',	''),
+(3,	0,	'F',	'Fulfilled (in the target bin)',	'');
+
