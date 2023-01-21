e70619ca001581932a6cd476c23cbfcb225ec1c5
Mark Page <ping@markpage.net>
2023-01-21 22:00:49 +0100

    Sets up Adminer tailored custom select views using CustomSelect

--- adminer-customselect/stores/customselect.cfg.php
+++ adminer-customselect/stores/customselect.cfg.php
@@ -1,18 +1,20 @@
 <?php
 return [
     'whereware' => [
-        'all_tables'            => '&where[0][col]=hidden&where[0][op]=%3D&where[0][val]=0',
+        'all_tables'                => '&where[0][col]=hidden&where[0][op]=%3D&where[0][val]=0',
         'per_table' => [
-/*
-            'ww_bin'            => '&where[1][col]=bin&where[1][op]=%3D',
-            'ww_composite'      => '&where[1][col]=sku&where[1][op]=%3D',
-            'ww_generic'        => '&where[1][col]=sku&where[1][op]=%3D',
-            'ww_location'       => '',
-            'ww_move'           => '&where[1][col]=from_location&where[1][op]=%3D&where[1][val]=W-1&where[2][col]=to_location&where[2][op]=%3D&where[2][val]=W-1&order[0]=id&desc[0]=1',
-            'ww_sku'            => '&where[1][col]=sku&where[1][op]=%3D',
-            'ww_status'         => '',
-            'ww_variant'        => '&where[1][col]=generic&where[1][op]=%3D'
-*/
+            'ww_bin'                => '&order[0]=bin',
+            'ww_booking'            => '&order[0]=id&desc[0]=1',
+            'ww_composite'          => '&order[0]=sku',
+            'ww_consignment'        => '&order[0]=id&desc[0]=1',
+            'ww_generic'            => '&order[0]=sku&order[1]=generic',
+            'ww_location'           => '&order[0]=location',
+            'ww_move'               => '&where[1][col]=cancelled&where[1][op]=%3D&where[1][val]=0&where[2][col]=status&where[2][op]=%3D&where[2][val]=F' 
+                                      .'&order[0]=updated&desc[0]=1&order[1]=order_ref&order[2]=booking_id&order[3]=sku',
+            'ww_project'            => '&order[0]=id&desc[0]=1',
+            'ww_sku'                => '&order[0]=sku',
+            'ww_recent_inventory'   => '&where[1][col]=location&where[1][op]=%3D&where[1][val]=W-1&order[0]=sku&order[1]=bin',
+            'ww_variant'            => '&order[0]=generic&order[1]=sku',
         ]
     ]
 ];
