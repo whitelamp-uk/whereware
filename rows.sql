54cc3606713028779f64d4ff2a5e04798472644b
Mark Page <ping@markpage.net>
2023-01-21 19:03:28 +0100

    Adds default data to version control

--- rows.sql
+++ rows.sql
@@ -1,6 +1,21 @@
 
-INSERT INTO `ww_status` (`id`, `hidden`, `status`, `name`, `notes`) VALUES
-(1,	0,	'R',	'Raised (still in the location of origin)',	''),
-(2,	0,	'T',	'In transit (between locations)',	''),
-(3,	0,	'F',	'Fulfilled (in the target bin)',	'');
+INSERT IGNORE INTO `ww_bin` (`updated`, `bin`, `notes`) VALUES
+(NOW(),	'',	'Any bin');
+
+INSERT IGNORE INTO `ww_location` (`updated`, `location`, `name`) VALUES
+(NOW(),	'',	'Any location of any type'),
+(NOW(),	'A-0',	'Any assembly location'),
+(NOW(),	'D-0',	'Any destination (eg customer) location'),
+(NOW(),	'I-0',	'Any goods-in location'),
+(NOW(),	'O-0',	'Any goods-out location'),
+(NOW(),	'S-0',	'Any source (eg supplier) location'),
+(NOW(),	'W-0',	'Any warehouse location');
+
+INSERT IGNORE INTO `ww_status` (`updated`, `status`, `name`) VALUES
+(NOW(),	'R',	'Raised (still in the location of origin)'),
+(NOW(),	'T',	'In transit (between locations)'),
+(NOW(),	'F',	'Fulfilled (in the target bin)');
+
+INSERT IGNORE INTO `ww_team` (`updated`, `team`, `name`) VALUES
+(NOW(),	'',	'Any team');
 
