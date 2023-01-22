68aae6c8c9fd4a700c37e5de22e05817475693e5
Mark Page <ping@markpage.net>
2023-01-22 14:04:03 +0100

    Fixes bug

--- adminer-customselect/stores/custom-select.cfg.php
+++ adminer-customselect/stores/custom-select.cfg.php
@@ -11,7 +11,7 @@ return [
             'ww_location'           => '&order[0]=location',
             'ww_move'               => '&where[1][col]=cancelled&where[1][op]=%3D&where[1][val]=0&where[2][col]=status&where[2][op]=%3D&where[2][val]=F' 
                                       .'&order[0]=updated&desc[0]=1&order[1]=order_ref&order[2]=booking_id&order[3]=sku',
-            'ww_project'            => '&order[0]=project&desc[0]=1',
+            'ww_project'            => '&order[0]=project',
             'ww_sku'                => '&order[0]=sku',
             'ww_recent_inventory'   => '&where[1][col]=location&where[1][op]=%3D&where[1][val]=W-1&order[0]=sku&order[1]=bin',
             'ww_variant'            => '&order[0]=generic&order[1]=sku',
